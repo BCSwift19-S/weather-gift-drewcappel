d5b24d6f63cf0a9c334e86ad9c00bee0f5ee31f1
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-03-18 12:02:42 -0400

    7.11 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Drew Cappel on 3/18/19.
+//  Copyright © 2019 Drew Cappel. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
