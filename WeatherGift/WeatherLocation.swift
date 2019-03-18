cf29c03e7013a30b8a9a5e313a5476c0504d9bca
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-03-18 14:39:20 -0400

    7.16 Complete

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -8,15 +8,27 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class WeatherLocation {
     var name = ""
     var coordinates = ""
+    var currentTemp = "--"
     
-    func getWeather() {
+    func getWeather(completed: @escaping () -> ()) {
         let weatherURL = urlBase + urlAPIkey + coordinates
+        
         Alamofire.request(weatherURL).responseJSON { response in
-            print(response)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                if let temperature = json["currently"]["temperature"].double {
+                    let roundedTemp = String(format: "%3.f", temperature)
+                    self.currentTemp = roundedTemp + "°"                }
+            case .failure(let error):
+                print(error)
+            }
+            completed()
         }
     }
 }
